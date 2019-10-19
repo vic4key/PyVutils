10f6c686ffb806cd3c0fe1d933a33f2f28ff818d
Vic P <vic4key@gmail.com>
2019-10-20 06:00:14 +0700

    Thread.Pool & Process.Pool

--- Process.py
+++ Process.py
@@ -3,10 +3,11 @@ from multiprocessing import Process, Queue, cpu_count
 POOL_BATCH_RESULT = "POOL_BATCH_RESULT"
 
 class Pool:
-    def __init__(self, nprocesses = None):
+    def __init__(self, nprocesses = None, debug = False):
         self._nprocesses = nprocesses if not nprocesses in [None, 0] else cpu_count()
         self._processes = []
         self._results = Queue()
+        self._debug = debug
         return
 
     def AddTask(self, fn, *args, **kwargs):
@@ -19,13 +20,44 @@ class Pool:
         num_list_args = len(listargs)
         if num_list_args == 0: return
 
-        num_items_per_batch = self._nprocesses if self._nprocesses < num_list_args else num_list_args
+        num_processes = self._nprocesses
+        if self._nprocesses > num_list_args: num_processes = num_list_args
 
-        num_processes = num_list_args // num_items_per_batch
-        if num_list_args % num_items_per_batch != 0: num_processes += 1
+        num_items_per_process = self._nprocesses if self._nprocesses < num_list_args else num_list_args
 
-        for i in range(0, num_list_args, num_items_per_batch):
-            batch_args = listargs[i : i + num_items_per_batch]
+        num_items_per_process = num_list_args // num_processes
+        num_items_left = num_list_args % num_processes
+
+        segments = []
+        for i in range(0, num_processes):
+            start = i * num_items_per_process
+            stop  = start + num_items_per_process
+            segments.append(stop)
+
+        idx_segment_head_for_combination = len(segments) - num_items_left
+
+        if num_items_left > 0:
+            for i, _ in enumerate(segments[idx_segment_head_for_combination:]):
+                segments[i + idx_segment_head_for_combination] += i + 1
+        del segments[-1]
+        segments.insert(0, 0)
+
+        if self._debug: print(f"Summary:"
+                              f"\n\t{num_list_args} items"
+                              f"\n\t{num_processes} processes"
+                              f"\n\t{num_items_per_process} items per process"
+                              f"\n\t{num_items_left} items are combined to last {num_items_left} processes"
+                              f"\n\tProcesses:")
+
+        for i, segment in enumerate(segments):
+
+            start = segments[i]
+            stop  = segments[i + 1] if i < num_processes - 1 else None
+
+            batch_args = listargs[start:stop]
+
+            if self._debug: print(f"\t\tProcess#i: {len(batch_args)} items")
+
             process = Process(target=self._fn_batch, args=[fn, self._results, batch_args])
             self._processes.append(process)
 
@@ -33,6 +65,8 @@ class Pool:
 
     def Launch(self, autoclose = True):
         result = []
+
+        if self._debug: print(f"\t{len(self._processes)} processes are created")
 
         for process in self._processes: process.start()
 
@@ -45,6 +79,8 @@ class Pool:
         for process in self._processes: process.join()
 
         if autoclose: self._results.close()
+
+        if self._debug: print(f"\t{len(self._processes)} processes are terminated")
 
         return result
 

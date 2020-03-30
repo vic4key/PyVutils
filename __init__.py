48581b5d094b76f9be866572dd46592fd6b14add
Vic P <vic4key@gmail.com>
2020-03-31 02:16:50 +0700

    __init__.py

--- __init__.py
+++ __init__.py
@@ -6,21 +6,20 @@
 @Name   : Vic Utility a.k.a Vutils
 '''''''''''''''''''''''''''''''''''
 
-__name__ = "PyVutils"
-
+__name__    = "PyVutils"
 __version__ = "1.0.0"
-
-__author__ = 'Vic P. <vic4key@gmail.com>'
-
-__all__ = [
+__author__  = 'Vic P. <vic4key@gmail.com>'
+__all__ =\
+[
     "Bytes",
-    "File",
-    "Thread",
-    "Process",
-    "Network",
     "Crypto",
-    "Math",
     "Cv",
-    "Utils",
+    "DCM",
+    "File",
+    "Math",
+    "Network",
     "Others",
+    "Process",
+    "Thread",
+    "Utils",
 ]

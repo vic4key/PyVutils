db58415caf600159eab3220defebb8d28fd41203
Vic P <vic4key@gmail.com>
2023-03-03 00:12:35 +0700

    Initial

--- PyVutils/Bytes.py
+++ PyVutils/Bytes.py
@@ -28,30 +28,30 @@ YiB = KiB**8
 # ---
 
 def format_bytes(number, unit = 1024) :
-    e = 0
-    l = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
-    if number > 0: e = int(math.log(number, unit))
-    if e < len(l): s = "%0.2f %s" % (number / unit**e, l[e])
-    # else: s = "%0.2f 10^%d" % (float(number) / unit**e, e)
-    return s
+  e = 0
+  l = ["B", "KiB", "MiB", "GiB", "TiB", "PiB", "EiB", "ZiB", "YiB"]
+  if number > 0: e = int(math.log(number, unit))
+  if e < len(l): s = "%0.2f %s" % (number / unit**e, l[e])
+  # else: s = "%0.2f 10^%d" % (float(number) / unit**e, e)
+  return s
 
 def qword_to_double(value) :
-    p = ctypes.pointer(ctypes.c_ulonglong(value))
-    p = ctypes.cast(p, ctypes.POINTER(ctypes.c_double))
-    return p.contents.value
+  p = ctypes.pointer(ctypes.c_ulonglong(value))
+  p = ctypes.cast(p, ctypes.POINTER(ctypes.c_double))
+  return p.contents.value
 
 def dword_to_float(value) :
-    p = ctypes.pointer(ctypes.c_ulong(value))
-    p = ctypes.cast(p, ctypes.POINTER(ctypes.c_float))
-    return p.contents.value
+  p = ctypes.pointer(ctypes.c_ulong(value))
+  p = ctypes.cast(p, ctypes.POINTER(ctypes.c_float))
+  return p.contents.value
 
 # ---
 
 def extract_bytes(number, position, n = 0) :
-    result = 0
-    if position < 0 and n > 0 : result = number & int("F"*n, 16)
-    else : result = number & int("FF"*position, 16)
-    return result
+  result = 0
+  if position < 0 and n > 0 : result = number & int("F"*n, 16)
+  else : result = number & int("FF"*position, 16)
+  return result
 
 # Casting. Eg. byte(0x1234) -> 0x34
 byte  = lambda number : extract_bytes(number, 1)
@@ -63,22 +63,19 @@ qword = lambda number : extract_bytes(number, 8)
 
 # def ParseStructure(pointer, struct) : return parse_structure(pointer, struct)
 
-class PEFormat(int, Enum):
-    UNKNOWN = -1
-    WIN     = 0
-    LINUX   = 1
-    MACHO   = 2
+class FileFormat(int, Enum):
+  UNKNOWN     = -1
+  PE_WIN      = 0
+  PE_LINUX    = 1
+  PE_MACHO    = 2
 
-def determine_pe_format(file_path: str) -> PEFormat:
-    result = PEFormat.UNKNOWN
-    try:
-      with open(file_path, "rb") as f:
-        data = f.read(7)
-        if data.startswith(bytearray.fromhex("4D5A90")):     # MZ
-          result = PEFormat.WIN
-        elif data.startswith(bytearray.fromhex("7F454C46")): # ELF
-          result = PEFormat.LINUX
-        elif data.startswith(bytearray.fromhex("CFFAEDFE")): # Mach-O
-          result = PEFormat.MACHO
-    except Exception as e: print(e)
-    return result
+def determine_file_format(file_path: str) -> FileFormat:
+  result = FileFormat.UNKNOWN
+  try:
+    with open(file_path, "rb") as f:
+      data = f.read(7)
+      if   data.startswith(bytearray.fromhex("4D5A")): result = FileFormat.PE_WIN
+      elif data.startswith(bytearray.fromhex("7F454C46")): result = FileFormat.PE_LINUX
+      elif data.startswith(bytearray.fromhex("CFFAEDFE")): result = FileFormat.PE_MACHO
+  except Exception as e: print(e)
+  return result

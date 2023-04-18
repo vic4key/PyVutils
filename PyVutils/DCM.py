67ed7ee97b7eff59310550deefc3dd02e848886a
Vic P <vic4key@gmail.com>
2023-04-18 13:39:39 +0700

    Initial

--- PyVutils/DCM.py
+++ PyVutils/DCM.py
@@ -6,17 +6,21 @@ import pydicom, glob
 import numpy as np
 from . import FS
 
-def load_dicom(file_path, force=True):
-    return pydicom.dcmread(file_path, force=force)
+def load_dicom(file_path, force_loading=True):
+    return pydicom.dcmread(file_path, force=force_loading)
 
-def load_dicom_directory(pattern, extensions = [], force=True):
+def load_dicom_directory(pattern, extensions = [], force_loading=True, image_only=False):
 
     list_ds = []
     def callback(file_path, file_directory, file_name):
         try:
-            list_ds.append(load_dicom(file_path, force))
+            ds = load_dicom(file_path, force_loading)
+            maybe_image  = False # assume an image always has z-location/position
+            maybe_image |= [0x0020, 0x1041] in ds # Slice Location
+            maybe_image |= [0x0020, 0x0032] in ds # Image Position
+            if not image_only or maybe_image:
+                list_ds.append(ds)
         except: print(f"Error when loading '{file_path}'")
-        return
     FS.recursive_directory(pattern, callback, extensions)
 
     dict_series = {}
@@ -33,16 +37,12 @@ def load_dicom_directory(pattern, extensions = [], force=True):
 
 def store_dicom_file(file_path, data_set, like_original = True):
     pydicom.filewriter.write_file(file_path, data_set, like_original)
-    return
 
 def save_dicom_file(file_path, data_set):
     data_set.save_as(file_path)
-    return
 
 def view_dicom_image(any_obj):
-
     import matplotlib.pyplot as plt
-
     obj_type = type(any_obj)
     if obj_type is str:
         DS = load_dicom(any_obj)
@@ -56,5 +56,3 @@ def view_dicom_image(any_obj):
         plt.show()
     else:
         print("ERROR: Unknown object type")
-
-    return
